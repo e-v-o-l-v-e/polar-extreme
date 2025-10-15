fef77bc824491e97fc92b7f5d38d70ef1c8df273
evolve <elivanoa5@gmail.com>
2025-10-15 14:45:00 +0200

    fixes buildings, door

--- scripts/buildings/Building.gd
+++ scripts/buildings/Building.gd
@@ -1,5 +1,4 @@
 class_name Building
-extends Resource
 
 enum BUILDING_TYPE { SCIENCE, UTILITY }
 
@@ -8,18 +7,10 @@ enum BUILDING_TYPE { SCIENCE, UTILITY }
 @export var building_name: String
 @export var building_description: String
 
-# visual
-@export var sprite: Texture2D
-
-# location
-@export var size: Vector2 		# from the the upper left tile of the building, starting at 0,0
-@export var building_position: Vector2
-
 @export var projets = []
 
-func constructs(pos: Vector2):
+func _init():
 	id = BuildingIdManager.register(self)
-	building_position = pos # upper left tile of the building
 
 func destroy():
 	BuildingIdManager.remove(id)
