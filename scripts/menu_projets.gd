c59bced336cbc1db3e4cf10764d58972dc0a26a2
DAUSSE CAPUCINE <capucine.dausse@etu.unistra.fr>
2025-11-11 19:36:22 +0100

    End menuScientists + rewrite of menuProjects

--- scripts/menu_projets.gd
+++ scripts/menu_projets.gd
@@ -1,11 +1,12 @@
 extends MarginContainer
 
 @export var projectScene : PackedScene
-@export var projectContainer : VBoxContainer
-@export var container : MarginContainer
-@export var text : Label
-@export var prev : Button
-@export var next : Button
+
+@onready var text_pas_projet: Label = $ninePatchRect/textPasProjet
+@onready var projet_container: VBoxContainer = $ninePatchRect/projetContainer
+
+@onready var prev: Button = $VBoxContainer/HBoxContainer/Prev
+@onready var next: Button = $VBoxContainer/HBoxContainer/Next
 
 var nbProject = 0;
 var nbPage = 1;
@@ -14,18 +15,12 @@ var nbProjPerPage = 0;
 
 var arrayProjects = Array()
 
-func pageAj() -> void :
-	if nbProject%5 != 0 :
-		nbProjPerPage = nbProject/5
-	else :
-		nbProjPerPage = 0
-
 
 func _process(delta: float) -> void:
 	if (nbProject == 0):
-		text.visible = true
+		text_pas_projet.visible = true
 	else :
-		text.visible = false
+		text_pas_projet.visible = false
 	
 	if (nbProject >= 5):
 		prev.visible = true
@@ -37,12 +32,6 @@ func _process(delta: float) -> void:
 
 func _on_prev_pressed() -> void:
 	if (nbPage > 1) :
-		#if (nbPage == nbPageTot) :
-		#	for i in nbProject%5 :
-		#		arrayProjects.get(i+5*(nbPage-1)).setVisibility(false)
-		#else :
-		#	for i in 5 :
-		#		arrayProjects.get(i+5*(nbPage-1)).setVisibility(false)
 		for proj in arrayProjects :
 			proj.setVisibility(false)
 			
@@ -78,15 +67,15 @@ func _on_button_test_pressed() -> void:
 		nbPageTot += 1
 
 	var project = projectScene.instantiate()
+	arrayProjects.append(project)
+	projet_container.add_child(project)
+	
 	project.setName(str(nbProject))
 	project.setVisibility(false)
 	
-	arrayProjects.append(project)
-	projectContainer.add_child(project)
 	if nbProjPerPage < 5 :
 		project.setVisibility(true)
 		nbProjPerPage += 1
-		print(nbProjPerPage)
 
 
 func _on_button_test_2_pressed() -> void:
@@ -94,7 +83,7 @@ func _on_button_test_2_pressed() -> void:
 		if (nbProject-1)%5 == 0 and nbProject > 1 :
 			nbPageTot -= 1
 		
-		projectContainer.remove_child(arrayProjects.get(nbProject-1))
+		projet_container.remove_child(arrayProjects.get(nbProject-1))
 		arrayProjects.remove_at(nbProject-1)
 		
 		nbProject -= 1
@@ -102,19 +91,4 @@ func _on_button_test_2_pressed() -> void:
 		if nbProjPerPage == 0 :
 			nbProjPerPage = 4
 			nbPageTot -= 1
-			print(str(nbProject) + " " + str(nbPageTot) + " " + str(nbPage) + " " + str(nbProjPerPage))
-			test()
-
-
-func test() -> void:
-	if (nbPage > 1) :
-		for proj in arrayProjects :
-			proj.setVisibility(false)
-			
-		nbPage -= 1
-		nbProjPerPage = 0
-		
-		for i in 5 :
-			arrayProjects.get(i+5*(nbPage-1)).setVisibility(true)
-			nbProjPerPage += 1
-			print(nbProjPerPage)
+			_on_prev_pressed()
